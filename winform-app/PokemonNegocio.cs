d948ea7c08968745f22b6e77c0989b9a7e62ede1
unknown <ivanewstigniejew2006@gmail.com>
2024-08-19 14:56:08 -0300

    primera subida

---
+++ winform-app/PokemonNegocio.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace winform_app
+{
+    internal class PokemonNegocio
+    {
+
+        public List<Pokemon> listar()
+        {
+            List<Pokemon> lista = new List<Pokemon>();
+            SqlConnection conexion = new SqlConnection();
+            SqlCommand comando = new SqlCommand();
+            SqlDataReader lector;
+            try
+            {
+                conexion.ConnectionString = "server=.\\SQLEXPRESS; database=POKEDEX_DB; integrated security=true";
+                comando.CommandType = System.Data.CommandType.Text;
+                comando.CommandText = "Select Numero, Nombre, Descripcion From POKEMONS";
+                comando.Connection = conexion;
+
+                conexion.Open();
+                lector = comando.ExecuteReader();
+
+                while (lector.Read())
+                {
+                    Pokemon aux = new Pokemon();
+                    aux.Numero = lector.GetInt32(0);
+                    aux.Nombre = (string)lector["Nombre"];
+                    aux.Descripcion = (string)lector["Descripcion"];
+
+                    lista.Add(aux);
+                }
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            finally { conexion.Close(); }
+        }
+
+    }
+}
